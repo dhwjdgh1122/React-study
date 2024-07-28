736ec2f1754e66c8294cb99672ad786aa9265bc0
ohjungho <dhwjdgh1122@naver.com>
2024-07-28 23:37:39 +0900

    teseaea

---
+++ 03-ecmascript-for-react/server.mjs
@@ -0,0 +1,11 @@
+const liveServer from 'live-server';
+
+const params = { 
+  host: "localhost", 
+    port: 3000, 
+    root: '.',
+    open: false,
+    mount: [['/', './public']], 
+};
+
+liveServer.start(params);
